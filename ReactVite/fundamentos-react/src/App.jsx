a733651ed1e581c8ab7faf713f4c52c7c01b5b95
Alexis Tapia <tapiaalexv@gmail.com>
2024-08-13 22:36:21 -0500

    Cambios en el proyecto

--- ReactVite/fundamentos-react/src/App.jsx
+++ ReactVite/fundamentos-react/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const MyButton = () => {
+    return (
+        <button>i am a button</button>
+    );
 }
 
-export default App
+const OnlineText = () => {
+    return(
+        <>
+            <h3>Online</h3>
+        </>
+    );
+}
+
+const OfflineText = () => {
+    return(
+        <>
+            <h3>Offline</h3>
+        </>
+    );
+}
+
+const App = () => {
+
+    const title = "Mi titulo desde una constante";
+    const classTitle = 'text-center';
+    const pathImg = "https://picsum.photos/200/300";
+    const user = true;
+
+    return (
+        <>
+            <h1 className={classTitle}>{title.toUpperCase()}</h1>
+            <img src={pathImg} alt={`imagen-${title}`}/>
+            <MyButton/>
+            {user ? <OnlineText/> : <OfflineText/> }
+            {user && <OfflineText/>}
+        </>
+    );
+}
+export default App;
+
+/*
+También se puede usar <div> </div>, sin embargo, react tiene <Fragment> que también puede ser vació <></>
+*/
