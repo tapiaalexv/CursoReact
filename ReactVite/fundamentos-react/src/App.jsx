4ca27c4d23992486ccdb83f21eee7f98c17f83b4
Alexis Tapia <tapiaalexv@gmail.com>
2024-08-13 23:19:50 -0500

    38. Desestructuración JS

--- ReactVite/fundamentos-react/src/App.jsx
+++ ReactVite/fundamentos-react/src/App.jsx
@@ -1,11 +1,26 @@
-const MyButton = () => {
+import imgDigimon from './assets/imgs/agumon.webp';
+import PropTypes from "prop-types";
+
+const MyButton = (props) => {
     return (
-        <button>i am a button</button>
+        <button>{props.text}</button>
     );
+}
+MyButton.propTypes = {
+    text: PropTypes.string.isRequired,
+}
+
+const MyButton2 = ({text}) => {
+    return (
+        <button>{text}</button>
+    );
+}
+MyButton2.propTypes = {
+    text: PropTypes.string.isRequired,
 }
 
 const OnlineText = () => {
-    return(
+    return (
         <>
             <h3>Online</h3>
         </>
@@ -13,7 +28,7 @@ const OnlineText = () => {
 }
 
 const OfflineText = () => {
-    return(
+    return (
         <>
             <h3>Offline</h3>
         </>
@@ -24,16 +39,32 @@ const App = () => {
 
     const title = "Mi titulo desde una constante";
     const classTitle = 'text-center';
-    const pathImg = "https://picsum.photos/200/300";
+    //const pathImg = "src/assets/imgs/agumon.webp";
     const user = true;
+    const fruts = ["🤡", "🥵", "😎"];
 
     return (
         <>
             <h1 className={classTitle}>{title.toUpperCase()}</h1>
-            <img src={pathImg} alt={`imagen-${title}`}/>
-            <MyButton/>
-            {user ? <OnlineText/> : <OfflineText/> }
+            <img src={imgDigimon} alt={`imagen-${title}`}/>
+            <MyButton text='btn 1' />
+            <MyButton text='btn 2' />
+            <MyButton text='btn 3' />
+            {user ? <OnlineText/> : <OfflineText/>}
             {user && <OfflineText/>}
+            <ul>
+                <li key={'0'}>{fruts[0]}</li>
+                <li key={'1'}>{fruts[1]}</li>
+                <li key={'2'}>{fruts[2]}</li>
+            </ul>
+            <ul>
+                {
+                    fruts.map((frut, index) => (
+                        <li key={index}>{frut} - {index}</li>
+                    ))
+                }
+            </ul>
+
         </>
     );
 }
